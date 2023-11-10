48609656ee9731a3de4f8d1f103f34529f6599d7
Riricola <71103639+Riricola@users.noreply.github.com>
2023-11-09 23:04:12 -0600

    upload

--- BlockChain.java
+++ BlockChain.java
@@ -1,4 +1,3 @@
-package MP7.src;
 
 /**
  * Our blockChain class stores nodes that contain Block data, and use node next to link each block to 
@@ -10,280 +9,277 @@ package MP7.src;
 public class BlockChain {
     
     
-/*---------------------------
- * Fields |
- * --------------------------
- */
-
-  //the previous block
-  Node first;
-
-  //the next block
-  Node last;
-
-  //the cursor block that will be used to iterate through the chain
-  Node current;
-
-  //determines the size of the chain
-  int size;
-
-
-/*---------------------------
- * Constructor |
- * --------------------------
- */
-
-  public BlockChain(int initial){
-    Block initialBlock = new Block (1, initial, new Hash(new byte[0]));
-    this.first = new Node(initialBlock, null);
-    current = first;
-    this.last = this.first;
-    this.size = 1;
-  }//BlockChain
-
-
- /*--------------------------
- * Methods |
- * --------------------------
- */
-
-  /*
-  * BlockMine()
-  * mines a new candidate block to be added to the end of the chain. 
-  * The returned Block should be valid to add onto this chain.
-  */
-  public Block mine(int amount){
-    //mine a block
-    Block mineBlock = new Block(this.getSize(), amount, this.last.data.getHash());
-    return mineBlock;
-    //check if its valid
-        
-  }//BlockMine
-
-  /*
-   * getSize()
-   * returns the size of the blockchain. Note that number of the blocks provides 
-   * a convenient method for quickly determining the size of the chain.
+  /*---------------------------
+   * Fields |
+   * --------------------------
    */
-  public int getSize(){
-    return size;
-  }//getSize
-
-  /*
-   * append()
-   * adds this block to the list, throwing an IllegalArgumentException if this block 
-   * cannot be added to the chain (because it is invalid wrt the rest of the blocks).
+  
+    //the previous block
+    Node first;
+  
+    //the next block
+    Node last;
+  
+    //the cursor block that will be used to iterate through the chain
+    Node current;
+  
+    //determines the size of the chain
+    int size;
+  
+  
+  /*---------------------------
+   * Constructor |
+   * --------------------------
    */
-  public void append(Block blk) throws IllegalArgumentException{
-    //sets the next node to the newly mined block
-    last.next = new Node(blk, null);
-
-    //checks if the appended block is valid or not
-    if(isValidBlockChain() == false){
-      //undo the pointer if invalid
-      last.next = null;
-      throw new IllegalArgumentException();
-    }
-
-    //connects the two 
-    last = last.next;
-
-    //points first.next to the last block if there are only 0-1 blocks
-    if(size <=1){
-      first.next = last;
-    }
-
-    this.size++;
-
-  }//append
-
-  /*
-   * removeLast()
-   * removes the last block from the chain, returning true. If the chain only contains 
-   * a single block, then removeLast does nothing and returns false.
+  
+    public BlockChain(int initial){
+      Block initialBlock = new Block (0, initial, null);
+      this.first = new Node(initialBlock, null);
+      current = first;
+      this.last = this.first;
+      this.size = 1;
+    }//BlockChain
+  
+  
+   /*--------------------------
+   * Methods |
+   * --------------------------
    */
-  public boolean removeLast(){
-    //if the chain is empty or has 1 block
-    if(this.size <= 1){
-      return false;
-    }//if
+  
+    /*
+    * BlockMine()
+    * mines a new candidate block to be added to the end of the chain. 
+    * The returned Block should be valid to add onto this chain.
+    */
+    public Block mine(int amount){
+      //mine a block
+      Block mineBlock = new Block(this.getSize(), amount, this.last.data.getHash());
+      return mineBlock;
+      //check if its valid
+          
+    }//BlockMine
+  
+    /*
+     * getSize()
+     * returns the size of the blockchain. Note that number of the blocks provides 
+     * a convenient method for quickly determining the size of the chain.
+     */
+    public int getSize(){
+      return size;
+    }//getSize
+  
+    /*
+     * append()
+     * adds this block to the list, throwing an IllegalArgumentException if this block 
+     * cannot be added to the chain (because it is invalid wrt the rest of the blocks).
+     */
+    public void append(Block blk) {
+      //sets the next node to the newly mined block
 
-    //remove block
-    last = null;//sets last block to the block before it
+      if(size<=1){
+        first.next = last;
+      }
 
-    //creates a node that will iterate to the end of the chain, and set last to it
-        
-    while(current.next != null){
-      current = current.next;
-    }//while
-
-    last = current;
-
-    size--;
-    return true;
-  }//removeLast
-
-  /*
-   * getHash()
-   * returns the hash of the last block in the chain
-   */
-  public Hash getHash(){
-    return last.getData().getHash();
-  }//getHash
-
-  /*
-   * isValidBlockChain()
-   * walks the blockchain and ensures that its blocks 
-   * are consistent and valid.
-   */
-  public boolean isValidBlockChain(){
-    //gets the starting balance
-    int totalAmt = first.data.getAmount();
-
-    //places current at the start of the chain
-    current = first;
-
-    //might have to check each blocks hash and mmake sure theyre in order
-    while(current.next != null){
-      if(current.data.hash.equals(current.next.data.prevHash)){
-        //calculates the transaction
-        totalAmt = current.data.getAmount() + current.next.data.getAmount();
-
-        //moves down the chain
-        current = current.next;
-      } else{
+      this.last.next = new Node(blk, null);
+      this.last = this.last.next;
+  
+      this.size++;
+  
+    }//append
+  
+    /*
+     * removeLast()
+     * removes the last block from the chain, returning true. If the chain only contains 
+     * a single block, then removeLast does nothing and returns false.
+     */
+    public boolean removeLast(){
+      //if the chain is empty or has 1 block
+      if(this.size <= 1){
         return false;
       }//if
-    }//while
-
-    //check if the transactions are valid
-    if(!(totalAmt >=0 || totalAmt <= 300)){
-      return false;
-    }//if
-    return true;
-  }//isValidBlockChain
-
-  /*
-   * printBalance()
-   * prints Alexis’s and Blake’s respective balances
-   */
-  public void printBalance(){
-    //saves the initial amount
-    int totalAmt = first.data.amount;
-    int alexis = totalAmt/2;
-    int blake = totalAmt/2;
-
-    current = first.next;
-    while(current.next != null){
-      totalAmt = current.data.getAmount();
-      
-        //check if the amount is negative or positive
-      if(totalAmt <= 0){
-        alexis += totalAmt; //subtract from alexis
-        blake += Math.abs(totalAmt); //add to blake
-      } else if(totalAmt >= 0){
-        alexis += totalAmt; //subtract from alexis
-        blake -= totalAmt; //add to blake
-      }//if
-
-      //move through the chain
-      current = current.next;
-    }//while
-
-    System.out("Alexis: " + alexis + ", Blake: " + blake);
-    }//printBalance
-
-  /*
-   * toString()
-   * returns a string representation of the BlockChain which is simply the
-   * string representation of each of its blocks, earliest to latest, one per line.
-   */
-  public String toString() {
-    String str = "";
-    current = first;
-
-    while (current != null) {
-      str.concat(current.block.toString()).concat("\n");
-      current = curent.next;
-    }//while
-    return str;
-  }//toString
-
-/*-----------------------
- * Helper functions|
- * ----------------------
- */
+  
+      //remove block
+      last = null;//sets last block to the block before it
+  
+      //creates a node that will iterate to the end of the chain, and set last to it
+          
+      while(current.next != null){
+        current = current.next;
+      }//while
+  
+      last = current;
+  
+      size--;
+      return true;
+    }//removeLast
+  
     /*
-     * checkAmt()
-     * iterates through the chain and makes sure the transactions are valid
+     * getHash()
+     * returns the hash of the last block in the chain
      */
-    /* 
-    public int checkAmt(){
-        //saves the initial amount
-        int totalAmt = first.data.amount;
-        int alexis = totalAmt/2;
-        int blake = totalAmt/2;
+    public Hash getHash(){
+      return last.getData().getHash();
+    }//getHash
+  
+    /*
+     * isValidBlockChain()
+     * walks the blockchain and ensures that its blocks 
+     * are consistent and valid.
+     */
+    public boolean isValidBlockChain(){
+      //gets the starting balance
+      int totalAmt = first.data.getAmount();
+  
+      //places current at the start of the chain
+      current = first;
+  
+      //might have to check each blocks hash and mmake sure theyre in order
+      while(current.next != null){
+        if(current.data.getHash().equals(current.next.data.prevHash)){
+          //calculates the transaction
+          totalAmt = current.data.getAmount() + current.next.data.getAmount();
+  
+          //moves down the chain
+          current = current.next;
+        } else{
+          return false;
+        }//if
+      }//while
+  
+      //check if the transactions are valid
+      if(!(totalAmt >=0 || totalAmt <= 300)){
+        return false;
+      }//if
+      return true;
+    }//isValidBlockChain
+  
+    /*
+     * printBalance()
+     * prints Alexis’s and Blake’s respective balances
+     */
+    public void printBalance(){
+      //saves the initial amount
+      int totalAmt = first.data.amount;
+      int alexis = totalAmt;
+      int blake = 0;
+  
+      current = first.next;
+      while(current.next != null){
+        totalAmt = current.data.getAmount();
+        
+          //check if the amount is negative or positive
+        if(totalAmt <= 0){
+          alexis += totalAmt; //subtract from alexis
+          blake += Math.abs(totalAmt); //add to blake
+        } else if(totalAmt >= 0){
+          alexis += totalAmt; //subtract from alexis
+          blake -= totalAmt; //add to blake
+        }//if
+  
+        //move through the chain
+        current = current.next;
+      }//while
+  
+      System.out.print("Alexis: " + alexis + ", Blake: " + blake);
+      }//printBalance
+  
+    /*
+     * toString()
+     * returns a string representation of the BlockChain which is simply the
+     * string representation of each of its blocks, earliest to latest, one per line.
+     */
+    public String toString() {
+      String str = "";
+      //current = first.next;
 
-        current = first.next;
-        while(current.next != null){
-            //totalAmt = current.data.getAmount() + current.next.data.getAmount();
-            if(current.data.getAmount() <= 0)
-            current = current.next;
-        }
-        return totalAmt;
-    }//checkAmt
-    */
+      if(size == 1){
+        return str.concat(first.getData().toString());
+      }
+      current = first;
 
-/*-----------------------
- * Inner class|
- * ----------------------
- */
-/**
-   * Nodes in the linked list.
+      while (current.next != null) {
+        str = str.concat(current.getData().toString());
+        str = str.concat("\n");
+        current = current.next;
+      }//while
+      return str;
+    }//toString
+  
+  /*-----------------------
+   * Helper functions|
+   * ----------------------
    */
-  class Node
-  {
-    // +--------+-----------------------------------------------------------
-    // | Fields |
-    // +--------+
-
-    /**
-     * The data stored in the node.
+      /*
+       * checkAmt()
+       * iterates through the chain and makes sure the transactions are valid
+       */
+      /* 
+      public int checkAmt(){
+          //saves the initial amount
+          int totalAmt = first.data.amount;
+          int alexis = totalAmt/2;
+          int blake = totalAmt/2;
+  
+          current = first.next;
+          while(current.next != null){
+              //totalAmt = current.data.getAmount() + current.next.data.getAmount();
+              if(current.data.getAmount() <= 0)
+              current = current.next;
+          }
+          return totalAmt;
+      }//checkAmt
+      */
+  
+  /*-----------------------
+   * Inner class|
+   * ----------------------
+   */
+  /**
+     * Nodes in the linked list.
      */
-    Block data;
-
-    /**
-     * The next node in the list.  Set to null at the end of the list.
-     */
-    Node next;
-
-    // +--------------+-----------------------------------------------------
-    // | Constructors |
-    // +--------------+
-
-    /**
-     * Create a new node with specified data and next.
-     */
-    public Node(Block data, Node next)
+    class Node
     {
-      this.data = data;
-      this.next = next;
-    } // Node(T, Node)
-
-    /**
-     * 
-     */
-    public Block getData(){
-        return this.data;
+      // +--------+-----------------------------------------------------------
+      // | Fields |
+      // +--------+
+  
+      /**
+       * The data stored in the node.
+       */
+      Block data;
+  
+      /**
+       * The next node in the list.  Set to null at the end of the list.
+       */
+      Node next;
+  
+      // +--------------+-----------------------------------------------------
+      // | Constructors |
+      // +--------------+
+  
+      /**
+       * Create a new node with specified data and next.
+       */
+      public Node(Block data, Node next)
+      {
+        this.data = data;
+        this.next = next;
+      } // Node(T, Node)
+  
+      /**
+       * 
+       */
+      public Block getData(){
+          return this.data;
+        } // getData()
+    
+      /**
+       * 
+       */
+      public Node getNext(){
+          return this.next;
       } // getData()
   
-    /**
-     * 
-     */
-    public Node getNext(){
-        return this.next;
-    } // getData()
-
-
-  } // class Node
-}
+  
+    } // class Node
+  }
--- BlockChainDriver.java
+++ BlockChainDriver.java
@@ -1,60 +1,98 @@
-package MP7.src;
-
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+
+/*
+ * Allows users to interact with our BlockChain
+ *
+ * @author Joyce Gill
+ * @author Maria Rodriguez
+ */
 
 public class BlockChainDriver {
-    public static void main(String[] args){
-        if(args.length > 1){
-            //if the user puts more args than just the dollar amount, give an error
-            return;
-        } else {
-            int amount = Integer.valueOf(args[0]);
-            Scanner scanner = new Scanner(System.in);
-            PrintWriter pen = new PrintWriter(System.out, true);
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+      if (args.length > 1) {
+        //if the user puts more args than just the dollar amount, give an error
+        System.err.println("Error: Incorrect number of arguments");
+      } else {
+        int amount = Integer.valueOf(args[0]);
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pen = new PrintWriter(System.out, true);
+  
+        //creates a blockChain with the given amount as the starting balance
+        BlockChain blockChain = new BlockChain(amount);
+        pen.println(blockChain.toString());
+  
+        pen.println("\nCommand?");
+        //read a user-inputted expression
+        String userInput = scanner.nextLine();
+        
 
-            //creates a blockChain with the given amount as the starting balance
-            BlockChain blockChain = new BlockChain(amount);
+        while (! userInput.equals("quit")) {
+                    
+          // keep reading lines
+          switch (userInput) {
+            case "mine": 
+              pen.println("Amount transferred?");
+              //pen.flush();
+              amount = Integer.parseInt(scanner.nextLine());
+              Block blk = blockChain.mine(amount);
+              pen.println("amount = " + amount + ", nonce = " + blk.getNonce() + '\n');
+              pen.flush();
+              break;
+            case "append": 
+              pen.println("Amount transferred?");
+              pen.flush();
+              amount = Integer.parseInt(scanner.nextLine());
+  
+              pen.println("Nonce?");
+              pen.flush();
+              int nonce = Integer.parseInt(scanner.nextLine());
+  
+              Block blk2 = new Block(blockChain.getSize(), amount, blockChain.getHash(), nonce);
+              /* Append the block */
+              blockChain.append(blk2);
+              pen.println('\n' + blockChain.toString());
+              break;  
+            case "remove": 
+              blockChain.removeLast();
+              break;
+            case "check": 
+              if (blockChain.isValidBlockChain()) {
+                pen.println("Chain is valid!");
+              }
+              else {
+                pen.println("Chain is invalid!");
+              }
+              break;
+            case "report": 
+              blockChain.printBalance();
+              break;
+            case "help": 
+              pen.println(menu());
+              break;
+            case "quit":
+              break;
+            default:
+              pen.println("Error: Invalid command. Type 'help' for valid commands.");
+          } // switch
 
-            //read a user-inputted expression
-            String userInput = scanner.nextLine();
-
-            while(! userInput.equals("quit")){
-                userInput = scanner.nextLine();
-                //keep reading lines
-                switch (userInput) {
-                    case "mine": Block blk = blockChain.mine(amount);
-                                    break;
-                    case "append": if(blk.hash.isValid()){
-                                    blockChain.append(blk);
-                                    } //needs a blk
-                                    break;
-                    case "remove": blockChain.removeLast();
-                                    break;
-                    case "check": blockChain.isValidBlockChain();
-                                    break;
-                    case "report": blockChain.printBalance();
-                                    break;
-                    case "help": pen.println(menu());
-                                    break;
-                    case "quit": userInput = scanner.nextLine();
-
-                }
-            }
-
-        }
-
-        //scan.close();
-    }//main
-
-    public static String menu(){
-        return String.join("Valid commands:",
-        "mine: discovers the nonce for a given transaction '\n'",
-        "append: appends a new block onto the end of the chain \n",
-        "remove: removes the last block from the end of the chain \n",
-        "check: checks that the block chain is valid \n",
-        "report: reports the balances of Alexis and Blake \n",
-        "help: prints this list of commands '\n'",
-        "quit: quits the program");
-    }
-}//blockDriver
+          pen.println('\n' + blockChain.toString());
+          pen.println("Command?");
+          userInput = scanner.nextLine();
+        } // while
+        scanner.close();
+      } // if/else
+      } // main
+  
+      public static String menu () {
+        return "Valid commands: " + '\n' +
+          "mine: discovers the nonce for a given transaction \n" +
+          "append: appends a new block onto the end of the chain \n" +
+          "remove: removes the last block from the end of the chain \n" +
+          "check: checks that the block chain is valid \n" +
+          "report: reports the balances of Alexis and Blake \n" +
+          "help: prints this list of commands \n" +
+          "quit: quits the program \n";
+      } // menu ()
+  } // class blockChainDriver 
